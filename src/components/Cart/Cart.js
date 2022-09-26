ab4b118d3f7e006440f9fedcf0ac9e999e043d6d
sartajdev007 <sartaj5akik@gmail.com>
2022-09-26 13:07:35 +0600

    order summary added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+    console.log(cart)
+
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    // 10% tax
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseInt(tax);
+
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summary</h3>
             <p>Selected Items: {cart.length}</p>
+            <p>Total Price: ${total}</p>
+            <p>Shipping: ${shipping}</p>
+            <p>Tax: {tax}</p>
+            <h4>Grand Total: {grandTotal}</h4>
         </div>
     );
 };
